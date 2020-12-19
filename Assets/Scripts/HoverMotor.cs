1beb73d7ea8002792d5733d0a8b4a92ed46ce60e
nickthorpe71 <nickthorpe71@gmail.com>
2020-12-19 10:54:05 -0600

    init commit

---
+++ Assets/Scripts/HoverMotor.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HoverMotor : MonoBehaviour
+{
+    public float speed = 90f;
+    public float hoverForce = 65f;
+    public float hoverHeight = 3.5f;
+
+    private float powerInput;
+    private Rigidbody bikeRigidbody;
+
+    private void Awake()
+    {
+        bikeRigidbody = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            powerInput = 1;
+        }
+        else
+        {
+            powerInput = 0;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        
+    }
+}
